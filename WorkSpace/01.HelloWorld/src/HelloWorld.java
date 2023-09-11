21bd1dbcefc02c40c80b04bdfe587f0479f17825
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-09-11 17:46:18 +0900

    자바첫날수업완료
    nain메소드? : 여기에서 작업하라고함
    syso : 콜솔창에 출력

---
+++ WorkSpace/01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+//주석 기계가 컴파일 하지 않는 코드 즉 인간이 보기 위한 설명
+//1. main : 메일메소드라고하며 윌가 당분간 계속해서 사용할 프로그램의 시작점
+//2. syso :프로그램이 진행되는 중에 어떤 문자나 결과를 출력하기 위한 명령어.
+//3. ctrl +space : 코드의 자동완성
+//4. ctrl +s 또는 ctrl +shift + s : 현재 파일 저장, 전체 파일저장
+
+// Q.나의 이니셜 Class를 하나 만들고  main메소드와 syso를 이용해서 콘솔에 내 이니셜이 출력되게
+//코딩해보기.
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("HelloWorldhse");
+}
+	
+
+	
+
+	
+}
+
---
+++ WorkSpace/01.HelloWorld/src/hse.java
@@ -0,0 +1,6 @@
+
+public class hse {
+	public static void main(String[] args) {
+		System.out.println("hse");
+	}
+}
