626ba03e24ef2ef5f17906bf72048885e12235d4
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-09-13 17:41:26 +0900

    1
    1

--- WorkSpace/02.Variable/src/pack02/variabletype/Ex05_Escape.java
+++ WorkSpace/02.Variable/src/pack02/variabletype/Ex05_Escape.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	//문자열 내부에 더블 코테이션을 콘솔에 출력하고싶음. "<-불가능함.
 	//출력을 할 수 없느 문자나 명령어르 통해서 문자열 정리등의 기능이 가능한것
  String str_v="\"자바\"를 좋아합니다..";//(\로 구분하여 가능하게함)
- 	System.out.println(str_v);
+ 	System.out.println(str_v);///////
  	//tab 	
  	System.out.println("번호\t이름\t직업  혈액형");
 	System.out.println("d:\\folder\\..");
