d1523f1261c3a4a9459b3614cc1e23cbdde8e130
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-09-18 17:43:30 +0900

    연산자
    연산

--- WorkSpace/03.Operator/src/Ex08_Oper.java
+++ WorkSpace/03.Operator/src/Ex08_Oper.java
@@ -9,13 +9,11 @@ public static void main(String[] args) {
 	--y;
 	y--;
 	z=x++;
-	z=++x;
+	z=x++;
 	z=++x +--y;
 	System.out.println(x);
 	System.out.println(y);
 	System.out.println(z);
 	
-	
-	System.out.println(70%6);
 }
 }
